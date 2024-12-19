1366fdc021cbe9ec4a9f6527b85c2582d848795c
fatima-ezzahra12 <amarzougfatimaezzahra@gmai.com>
2024-12-19 15:21:47 -0800

    cycle 1 : FizzBuzz.de(1)  ‘1’

---
+++ src/test/java/com.controle.tdd/FizzBuzzTest.java
@@ -0,0 +1,16 @@
+package com.controle.tdd;
+import org.junit.jupiter.api.Test;
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+public class FizzBuzzTest {
+    @Test
+    void fizzbuzz() {
+       assertEquals("1",FizzBuzz.de(1));
+    }
+}
+
+
+
+
+
+
---
+++ src/test/java/com.controle.tdd/Main.java
@@ -0,0 +1,12 @@
+package com.controle.tdd;
+
+public class Main {
+
+    public static void main(String[] args) {
+        System.out.println("Hello World");
+        for (int i = 1; i <= 20; i++) {
+            System.out.println(FizzBuzz.de(i));
+        }
+    }
+}
+
